185b282d6487ed1d5f708b3535b27ca3b3651f07
Jason Shapiro <jshapiro651@gmail.com>
2022-05-31 18:05:18 -0500

    Added functionality to store ETH in contract and use that ETH in a transaction

--- Contracts/LimitOrder.sol
+++ Contracts/LimitOrder.sol
@@ -1,25 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-pragma abicoder v2;
 
 import "https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/interfaces/ISwapRouter.sol";
-import "https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/interfaces/IQuoter.sol";
 
 interface IUniswapRouter is ISwapRouter {
     function refundETH() external payable;
 }
 
 contract LimitOrderV3 {
-    IUniswapRouter public constant uniswapRouter =
+    IUniswapRouter private constant uniswapRouter =
         IUniswapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-    IQuoter public constant quoter =
-        IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
     address private constant fintechBootcampProj3Token =
         0x74B656031DfBD104dAdFB9ac0A2A620A4170b9e7;
     address private constant WETH9 = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
+    address payable owner;
+    uint256 public balance = 0;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyFBP3TfromAccount(uint256 amountIn) external {
+        uint256 deadline = block.timestamp + 15;
+        address tokenIn = WETH9;
+        address tokenOut = fintechBootcampProj3Token;
+        uint24 fee = 3000;
+        address recipient = address(this);
+        uint256 amountOutMinimum = 1;
+        uint160 sqrtPriceLimitX96 = 0;
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams(
+                tokenIn,
+                tokenOut,
+                fee,
+                recipient,
+                deadline,
+                amountIn,
+                amountOutMinimum,
+                sqrtPriceLimitX96
+            );
+
+        uniswapRouter.exactInputSingle(params);
+        uniswapRouter.refundETH();
+
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(
+            success,
+            "Attempted to refund remaining ETH to sender, but failed"
+        );
+    }
 
     function convertExactEthToFBP3T() external payable {
-        require(msg.value > 0, "Must pass non 0 ETH amount");
+        require(msg.value > 0, "Must include ETH in the request");
 
         uint256 deadline = block.timestamp + 15;
         address tokenIn = WETH9;
@@ -45,10 +78,24 @@ contract LimitOrderV3 {
         uniswapRouter.exactInputSingle{value: msg.value}(params);
         uniswapRouter.refundETH();
 
-        // refund leftover ETH to user
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
-        require(success, "refund failed");
+        require(
+            success,
+            "Attempted to refund remaining ETH to sender, but failed"
+        );
     }
 
-    receive() external payable {}
+    function withdraw(uint256 amount, address payable recipient) public {
+        require(recipient == owner, "You do not own this account");
+        balance = address(this).balance - amount;
+        return recipient.transfer(amount);
+    }
+
+    function deposit() public payable {
+        balance = address(this).balance;
+    }
+
+    receive() external payable {
+        balance = address(this).balance;
+    }
 }
