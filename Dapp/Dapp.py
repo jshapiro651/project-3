c708e6b0744b4e382a57858719d3092fdbf8c5dd
Tim <94910844+timothy-wardlow@users.noreply.github.com>
2022-06-03 14:51:33 -0500

    testing Dapp

--- Dapp/Dapp.py
+++ Dapp/Dapp.py
@@ -21,9 +21,9 @@ w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 # Define the load_contract function
 def load_contract():
 
-    # Load Art Gallery ABI
-    with open(Path('./contracts/compiled/certificate_abi.json')) as f:
-        certificate_abi = json.load(f)
+    # Load ABI
+    with open(Path('compiled_LimitOrder.json')) as f:
+        compiled_LimitOrder = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
     contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
@@ -31,7 +31,7 @@ def load_contract():
     # Get the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=certificate_abi
+        abi=compiled_LimitOrder
     )
     # Return the contract from the function
     return contract
@@ -45,22 +45,22 @@ contract = load_contract()
 # Award Certificate
 ################################################################################
 
-accounts = w3.eth.accounts
-account = accounts[0]
-student_account = st.selectbox("Select Account", options=accounts)
-certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
-if st.button("Award Certificate"):
-    contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
+#accounts = w3.eth.accounts  ## gettting all the accounts from ganache 
+#account = accounts[0]       ## by default display the 1st account 
+#student_account = st.selectbox("Select Account", options=accounts)
+#certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
+#if st.button("Award Certificate"):
+#    contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
 
 ################################################################################
 # Display Certificate
 ################################################################################
-certificate_id = st.number_input("Enter a Certificate Token ID to display", value=0, step=1)
-if st.button("Display Certificate"):
+#buy_FBP3T = st.number_input("Enter a Certificate Token ID to display", value=0, step=1)
+#if st.button("Display Certificate"):
     # Get the certificate owner
-    certificate_owner = contract.functions.ownerOf(certificate_id).call()
-    st.write(f"The certificate was awarded to {certificate_owner}")
+#    certificate_owner = contract.functions.ownerOf(certificate_id).call()
+#    st.write(f"The certificate was awarded to {certificate_owner}")
 
     # Get the certificate's metadata
-    certificate_uri = contract.functions.tokenURI(certificate_id).call()
-    st.write(f"The certificate's tokenURI metadata is {certificate_uri}")
+#    certificate_uri = contract.functions.tokenURI(certificate_id).call()
+ #   st.write(f"The certificate's tokenURI metadata is {certificate_uri}")
