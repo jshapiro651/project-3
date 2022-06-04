714dd110eb6fb6df048df7c17f21d650db7ba408
Tim <94910844+timothy-wardlow@users.noreply.github.com>
2022-06-04 09:57:46 -0500

    updating Dapp.py

--- Dapp/Dapp.py
+++ Dapp/Dapp.py
@@ -1,6 +1,9 @@
 import os
 import json
 from web3 import Web3
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3.middleware import construct_sign_and_send_raw_middleware
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
@@ -10,28 +13,27 @@ load_dotenv()
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
-################################################################################
-# Contract Helper function:
-# 1. Loads the contract once using cache
-# 2. Connects to the contract using the contract address and ABI
-################################################################################
+# Set the contract address (this is the address of the deployed contract)
+contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+
+# Set the public account
+pub_account = os.getenv("ACCOUNT")
 
 # Cache the contract on load
+
+
 @st.cache(allow_output_mutation=True)
 # Define the load_contract function
 def load_contract():
 
-    # Load ABI
-    with open(Path('compiled_LimitOrder.json')) as f:
-        compiled_LimitOrder = json.load(f)
-
-    # Set the contract address (this is the address of the deployed contract)
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+    # Load Limit Order ABI
+    with open(Path('./limit_order_abi.json')) as f:
+        contract_abi = json.load(f)
 
     # Get the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=compiled_LimitOrder
+        abi=contract_abi
     )
     # Return the contract from the function
     return contract
@@ -40,33 +42,32 @@ def load_contract():
 # Load the contract
 contract = load_contract()
 
-# Streamlit Header
-st.markdown('Uniswap Limit Order Project')
+# Display balance
+balance = contract.functions.balance().call()
+st.write(f"The balance is {balance}")
 
-if st.sidebar.button("Buy FBP3T"):
-    contract.functions.buyFBP3TfromAccount()
+# https://web3py.readthedocs.io/en/latest/web3.eth.account.html#read-a-private-key-from-an-environment-variable
+private_key = os.getenv("PRIVATE_KEY")
+assert private_key is not None, "You must set PRIVATE_KEY environment variable"
+assert private_key.startswith(
+    "0x"), "Private key must start with 0x hex prefix"
 
+account: LocalAccount = Account.from_key(private_key)
+# w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
 
-################################################################################
-# Award Certificate
-################################################################################
+if st.button("Swap ETH for FBP3T"):
+    nonce = w3.eth.get_transaction_count(pub_account)
+    txn = contract.functions.buyFBP3TfromAccount(156520000000000).buildTransaction({
+        'chainId': 42,
+        'gas': 3000000,
+        'maxFeePerGas': w3.toWei('10', 'gwei'),
+        'maxPriorityFeePerGas': w3.toWei('10', 'gwei'),
+        'nonce': nonce,
+    })
+    signed_txn = account.signTransaction(txn)
+    w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-#accounts = w3.eth.accounts  ## gettting all the accounts from ganache 
-#account = accounts[0]       ## by default display the 1st account 
-#student_account = st.selectbox("Select Account", options=accounts)
-#certificate_details = st.text_input("Certificate Details", value="FinTech Certificate of Completion")
-#if st.button("Award Certificate"):
-#    contract.functions.awardCertificate(student_account, certificate_details).transact({'from': account, 'gas': 1000000})
-
-################################################################################
-# Display Certificate
-################################################################################
-#buy_FBP3T = st.number_input("Enter a Certificate Token ID to display", value=0, step=1)
-#if st.button("Display Certificate"):
-    # Get the certificate owner
-#    certificate_owner = contract.functions.ownerOf(certificate_id).call()
-#    st.write(f"The certificate was awarded to {certificate_owner}")
-
-    # Get the certificate's metadata
-#    certificate_uri = contract.functions.tokenURI(certificate_id).call()
- #   st.write(f"The certificate's tokenURI metadata is {certificate_uri}")
+# This doesn't work...
+# if st.button("Swap ETH for FBP3T"):
+#     contract.functions.buyFBP3TfromAccount(
+#         156520000000000).transact({'gas': 1000000})
