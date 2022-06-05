1a62041f8ec997686b46ae9a7c0edda520c11724
kearn082 <jonathankearney10@gmail.com>
2022-06-05 18:21:31 -0500

    Added button that links to Etherscan in order to verify that the transaction was successful.

--- Dapp/dapp.py
+++ Dapp/dapp.py
@@ -1,3 +1,4 @@
+from email.headerregistry import Address
 import os
 import json
 from web3 import Web3
@@ -7,42 +8,45 @@ from web3.middleware import construct_sign_and_send_raw_middleware
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import webbrowser
 
-load_dotenv()  ## import env variables in .env files 
+load_dotenv()
 
-# Define and connect a new Web3 provider. Copied URL from remix. Might need to just copy from the compiler if this does not work
-w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))   ## connection to Infura
+# Define and connect a new Web3 provider
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
-# Set the contract address to the address of the deployed contract from Remix
+# Set the contract address (this is the address of the deployed contract)
 contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
 
 # Set the public account
 pub_account = os.getenv("ACCOUNT")
 
 # Cache the contract on load
-@st.cache(allow_output_mutation=True)     ## cache the smart contract in the memory of the physical device running the frontend  
+
+
+@st.cache(allow_output_mutation=True)
 # Define the load_contract function
 def load_contract():
 
-    # ABIC:\Users\jonat\project-3\Dapp\compiled_LimitOrder.json
-    with open(Path('./limit_order_abi.json')) as f:   ## defining the path to the backend
-        compiled_LimitOrder = json.load(f)
-
-    # Set the contract address (this is the address of the deployed contract)
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")  ## address of the smart contract (backend)
+    # Load Limit Order ABI
+    with open(Path('./LimitOrder_ABI.json')) as f:
+        contract_abi = json.load(f)
 
     # Get the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=compiled_LimitOrder
+        abi=contract_abi
     )
     # Return the contract from the function
     return contract
 
 
 # Load the contract
-contract = load_contract()   ## loading contract and calling 'contract' 
+contract = load_contract()
 
+# Display balance
+balance = contract.functions.balance().call()
+#st.write(f"The balance is {balance}")
 
 # https://web3py.readthedocs.io/en/latest/web3.eth.account.html#read-a-private-key-from-an-environment-variable
 private_key = os.getenv("PRIVATE_KEY")
@@ -53,24 +57,40 @@ assert private_key.startswith(
 account: LocalAccount = Account.from_key(private_key)
 # w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
 
-# Streamlit Title
-st.markdown("# Limit Order Project")
-st.text("\n")
+st.markdown("# Uniswap Limit Order Project")
+st.markdown("## Welcome to our DApp!")
+st.text(" \n")
 
-st.sidebar.markdown("## Interact with the Contract")
+st.sidebar.markdown(
+    "## Check Balance | Withdraw | Deposit | Swap ETH for FBP3T | Validate Transaction")
+
+# Check Balance button
+
+if st.sidebar.button("Check Balance (wei)"):
+    st.sidebar.write(f"The balance is {balance}")
 
 
-# Display balance
-balance = contract.functions.balance().call()
-st.write(f"The balance is {balance} WEI")
+# Withdraw button
+amount = st.sidebar.number_input("Input amount of ETH to withdraw")
+amount = w3.toWei(amount, 'ether')
+# Create a button that calls the `send_transaction` function and returns the transaction hash
+if st.sidebar.button("Withdraw"):
+    nonce = w3.eth.get_transaction_count(pub_account)
+    w_txn = contract.functions.withdraw(amount, pub_account).buildTransaction({
+        'chainId': 42,
+        'gas': 3000000,
+        'maxFeePerGas': w3.toWei('10', 'gwei'),
+        'maxPriorityFeePerGas': w3.toWei('10', 'gwei'),
+        'nonce': nonce,
+    })
 
-# Check Balance button on the sidebar
-if st.sidebar.button("Check Balance (WEI)"):
-    st.write(f"The balance is {balance} WEI")
+    signed_txn = account.signTransaction(w_txn)
+    w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-swap_amount = st.sidebar.number_input("Input the amount of ETH you would like to swap.")
-
-#Swap ETH for FBP3T button on the sidebar
+# Swap button
+swap_amount = st.sidebar.number_input(
+    "Input the amount of ETH you'd like to swap for FBP3T")
+swap_amount = w3.toWei(swap_amount, 'ether')
 if st.sidebar.button("Swap ETH for FBP3T"):
     nonce = w3.eth.get_transaction_count(pub_account)
     txn = contract.functions.buyFBP3TfromAccount(swap_amount).buildTransaction({
@@ -83,30 +103,9 @@ if st.sidebar.button("Swap ETH for FBP3T"):
     signed_txn = account.signTransaction(txn)
     w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-
-
-# This doesn't work...
-# if st.button("Swap ETH for FBP3T"):
-#     contract.functions.buyFBP3TfromAccount(
-#         156520000000000).transact({'gas': 1000000})
-
-#amount = st.sidebar.number_input("Amount to Withdraw")
-#payable_recipient = "0x74B656031DfBD104dAdFB9ac0A2A620A4170b9e7"
-# Withdraw button on the sidebar
-#if st.sidebar.button("Withdraw"):
-    #amount = st.sidebar.number_input("Amount to Withdraw")
-    #recipient = st.sidebar.text_input("Input Recipeint's Address")
-    #nonce = w3.eth.get_transaction_count(pub_account)
-    #w_txn = contract.functions.withdraw(amount, payable_recipient).buildTransaction({
-        #'chainId': 42,
-        #'gas': 3000000,
-        #'maxFeePerGas': w3.toWei('10', 'gwei'),
-        #'maxPriorityFeePerGas': w3.toWei('10', 'gwei'),
-        #'nonce': nonce,
-    #})
-    #w_signed_txn = account.signTransaction(w_txn)
-    #w3.eth.send_raw_transaction(w_signed_txn.rawTransaction)
-        
-
-# $ streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
-# How to pass a URL to streamlit
+# Transaction Validation
+#st.sidebar.write("Confirm your transaction: https://kovan.etherscan.io/address/0x44bde79162d767da1f12ec8f5c16934ed48f1402 ")
+url = "https://kovan.etherscan.io/address/0x44bde79162d767da1f12ec8f5c16934ed48f1402"
+st.sidebar.markdown('## Confirm your Transaction') 
+if st.sidebar.button('Open Etherscan'):
+    webbrowser.open_new_tab(url)
